2bd85f029cd79862c73ec4eb717248d149f6338d
Aanya <114793986+AanyaPanigrahi@users.noreply.github.com>
2022-12-28 23:40:51 +0530

    Update class20.py

--- class20.py
+++ class20.py
@@ -77,5 +77,27 @@ it.__next__() #1
 it.__next__() #2
 it.__next__() #3
 it.__next__() #4
+'''
+Stop the iteration
+'''
 
+class myrange:
+    def __init__(self,n):
+        self.n=n
+    def __iter__(self):
+        return myrange_iterator(self)
+class myrange_iterator:
+    def __init__(self,myrange):
+        self.myrange=myrange
+        self.i=0
+    def __next__(self):
+        ret=self.i
+        self.i+=1
+        if ret>=self.myrange.n:
+            raise StopIteration
+        return ret
+
+a=myrange(5)
+it=iter(a)
+next(it)
     
